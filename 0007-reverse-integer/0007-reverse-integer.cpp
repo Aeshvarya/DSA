e140d711bcda8f8f3b61012be767cfe760962507
Aeshvarya Awasthi <aeshvarya310305@gmail.com>
2026-06-11 23:27:00 +0530

    Time: 6 ms (6.16%), Space: 8.4 MB (82.78%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int reverse(int x) {
 
-        int ns = 0;
+        int numb = 0;
 
         while(x != 0){  
 
             int ks = x % 10;
-            if(ns > INT_MAX/10 || ns < INT_MIN/10){
+            if(numb > INT_MAX/10 || numb < INT_MIN/10){
                 return 0;
             }
-            ns = ns * 10 + ks;
+            numb = numb * 10 + ks;
             x /= 10;
         }
- return ns;
+ return numb;
     }
 };
