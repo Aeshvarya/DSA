02eef4010e25e96878b89ebbf02be9d736a73634
Aeshvarya Awasthi <aeshvarya310305@gmail.com>
2026-06-16 21:05:09 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int firstb(vector<int>& nums, int target){
-        int low = 0;
+        int lowerow = 0;
         int high = nums.size() - 1;
         int ans = -1;
 
-        while(low <= high){
-            int mid = (high + low) / 2;
+        while(lowerow <= high){
+            int mid = (high + lowerow) / 2;
 
             if(nums[mid] >= target){
                 ans = mid;
                 high = mid - 1;
             }
             else{
-                low=mid+1;
+                lowerow=mid+1;
             }
         }
 
         return ans;
     }
 
-    int lastb(vector<int>& nums, int target){
-        int low = 0;
+    int lowerastb(vector<int>& nums, int target){
+        int lowerow = 0;
         int high = nums.size() - 1;
        int ans = nums.size();
 
-        while(low <= high){
-            int mid = (high +low) / 2;
+        while(lowerow <= high){
+            int mid = (high +lowerow) / 2;
 
             if(nums[mid] > target){
                 ans = mid;
                 high=mid-1;
             }
             else{
-                low=mid+1;
+                lowerow=mid+1;
             }
         }
 
@@ -41,12 +41,12 @@ public:
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb=firstb(nums,target);
-        int ub=lastb(nums,target);
-        if(lb==-1 || nums[lb]!=target){
+        int lowerb=firstb(nums,target);
+        int upperb=lowerastb(nums,target);
+        if(lowerb==-1 || nums[lowerb]!=target){
             return {-1,-1};
         }
 
-        return {lb,ub-1};
+        return {lowerb,upperb-1};
     }
 };
