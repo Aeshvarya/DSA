fd7d4044f64f537815baa11319fc629f75ed0907
Aeshvarya Awasthi <aeshvarya310305@gmail.com>
2026-07-06 11:54:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (76.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,15 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return false;
         vector<int>arr1(26,0);
-        vector<int>arr2(26,0);
         for(int i=0;i<s.size();i++){
             arr1[s[i]-'a']++;
         }
         for(int i=0;i<t.size();i++){
-            arr2[t[i]-'a']++;
+            arr1[t[i]-'a']--;
         }
         for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]){
+            if(arr1[i]!=0){
                 return false;
             }
         }
